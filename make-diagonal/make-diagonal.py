73cc20d532db63d9f0088bd99d0b45af47f44132
PrakarshaKondour <kondourprakarsha@gmail.com>
2026-02-18 16:23:52 +0530

    Update solution for make-diagonal

--- make-diagonal/make-diagonal.py
+++ make-diagonal/make-diagonal.py
@@ -6,6 +6,7 @@ def make_diagonal(v):
     """
     # Write code here
     v1=np.zeros((len(v),len(v)))
+    v1=np.diag(v)
     for i in range(len(v1)):
         for j in range(len(v1)):
             if(i==j):
